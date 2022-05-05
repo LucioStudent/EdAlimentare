059be15dc4630e0e2c4fb1d30b1b010e26a39b6f
LucioStudent <82449494+LucioStudent@users.noreply.github.com>
2022-05-05 18:28:57 +0200

    Add files via upload
    Abbiamo aggiunto dei bottoni che permettono di cambiare il line char per visualizzare nel grafico il totale dei panini per una determinata classe, è stato sistemato anche il css

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ED.Alimentare</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load("current", {packages:["corechart"]});
@@ -18,7 +19,7 @@
           ['Classe', 'TotPaniniClasse'],
           <?php
             $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-            $query = "SELECT classe, sum(tot) as tot FROM panini_n_csv group by classe";
+            $query = "SELECT classe, sum(tot) as tot FROM panini_n_csv where classe <> 'S' group by classe ";
             $result = $connection->query($query);
             if ($result->num_rows != 0) {
               $ntmp = "";
@@ -47,20 +48,40 @@
         var data = google.visualization.arrayToDataTable([
           ['Panini', 'Quantità Ordinata'],
           <?php
-            $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-            $query = "SELECT `data`, sum(tot) as tot FROM panini_n_csv group by `data`";
-            $result = $connection->query($query);
-            if ($result->num_rows != 0) {
-              $ntmp = "";
-              while ($row = $result->fetch_array()) {
-                  echo "['$row[data]',$row[tot]],";
+            if (isset($_GET['classe'])){
+              $queryAdd = $_GET['classe'];
+              $strTmp = "where classe <> 'S' and classe = '";
+              $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+              $query = "SELECT `data`, sum(tot) as tot FROM panini_n_csv ".$strTmp.$queryAdd."' group by `data`";
+              $result = $connection->query($query);
+              if ($result->num_rows != 0) {
+                while ($row = $result->fetch_array()) {
+                    echo "['$row[data]',$row[tot]],";
+                }
+              }
+            }
+            else{
+              $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+              $query = "SELECT `data`, sum(tot) as tot FROM panini_n_csv group by `data`";
+              $result = $connection->query($query);
+              if ($result->num_rows != 0) {
+                while ($row = $result->fetch_array()) {
+                    echo "['$row[data]',$row[tot]],";
+                }
               }
             }
           ?>
         ]);
 
         var options = {
-          title: 'Totale ordinazioni per ogni data',
+          <?php
+          if (isset($_GET['classe'])){
+            echo "title: 'Totale ordinazioni per Classe ".$_GET['classe']."',";
+          }
+          else{
+            echo "title: 'Totale ordinazioni per ogni data',";
+          }
+          ?>
           curveType: 'function',
           legend: { position: 'rigth' }
         };
@@ -74,5 +95,20 @@
 <body>
     <div id="piechart_3d"></div>
     <div id="curve_chart"></div>
+    <div class="box">
+      <h3>Visualizza grafico temporale per classi:</h3>
+      <br>
+    <?php
+      $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+      $query = "SELECT DISTINCT classe FROM panini_n_csv where classe <> 'S';";
+      $result = $connection->query($query);
+      if ($result->num_rows != 0) {
+        while ($row = $result->fetch_array()) {
+            echo "<a href='./index.php?classe=$row[classe]' class='btn btn-outline-primary'>Classe $row[classe]</a>";
+        }
+      }
+    ?>
+    <a href='./index.php' class='btn btn-outline-primary'>Visualizza totale</a>
+    </div>
 </body>
 </html>
